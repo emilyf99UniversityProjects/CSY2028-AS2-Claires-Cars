964d14415920b25fdbfbfe071906dcb6c11bf705
EmilyF99 <emilyfletcher099@gmail.com>
2021-12-22 22:15:01 +0000

    split into left and right content addManufacturer

--- websites/cars/public/addmanufacturer.php
+++ websites/cars/public/addmanufacturer.php
@@ -5,22 +5,22 @@ session_start();
 ?> */
 
 	$title = 'Claires\'s Cars - Admin';
-	$content =
-		'<main class="admin">
-
-		<section class="left">
+	$mainContent =
+		'<main class="admin">';
+	$leftContent = 
+		'<section class="left">
 			<ul>
 				<li><a href="manufacturers.php">Manufacturers</a></li>
 				<li><a href="cars.php">Cars</a></li>
 			</ul>
-		</section>
-
-		<section class="right">
+		</section>';
+	$rightContent = 
+		'<section class="right">
 	
 		</section>
 		</main>';
 
-	require '../templates/layout.html.php';
+	require '../templates/adminlayout.html.php';
 
 	/*
 	<?php
