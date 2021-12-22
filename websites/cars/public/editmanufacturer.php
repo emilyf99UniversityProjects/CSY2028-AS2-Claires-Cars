4b0c689cc944e9be433240c95e7afd928845a2c3
EmilyF99 <emilyfletcher099@gmail.com>
2021-12-22 23:02:46 +0000

    removed redundancy editManu.php

--- websites/cars/public/editmanufacturer.php
+++ websites/cars/public/editmanufacturer.php
@@ -1,36 +1,12 @@
 <?php
-$pdo = new PDO('mysql:dbname=cars;host=mysql', 'student', 'student');
-session_start();
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="/styles.css"/>
-		<title>Claires's Cars - Admin</title>
-	</head>
-	<body>
-	<header>
-		<section>
-			<aside>
-				<h3>Opening Hours:</h3>
-				<p>Mon-Fri: 09:00-17:30</p>
-				<p>Sat: 09:00-17:00</p>
-				<p>Sun: Closed</p>
-			</aside>
-			<img src="/images/logo.png"/>
+	/*
+	$pdo = new PDO('mysql:dbname=cars;host=mysql', 'student', 'student');
+	session_start(); */
 
-		</section>
-	</header>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/cars.php">Showroom</a></li>
-			<li><a href="/about.html">About Us</a></li>
-			<li><a href="/contact.php">Contact us</a></li>
-		</ul>
-	</nav>
-		<img src="/images/randombanner.php"/>
-	<main class="admin">
+	$title = 'Claires\'s Cars - Admin';
+
+	$content= 
+	'<main class="admin">
 
 	<section class="left">
 		<ul>
@@ -41,10 +17,13 @@ session_start();
 	</section>
 
 	<section class="right">
-	
-		
-	<?php
+	</section>
+	</main>';
 
+	require '../templates/layout.html.php';
+	
+	/*
+	<?php
 
 	if (isset($_POST['submit'])) {
 
@@ -98,14 +77,7 @@ session_start();
 		<?php
 		}
 
-	}
+	} */
 	?>
 
-
-</section>
-	</main>
-	<footer>
-		&copy; Claire's Cars 2018
-	</footer>
-</body>
-</html>
+	
