ae35625317eb861deb357b73b4ffb737fccade5b
EmilyF99 <emilyfletcher099@gmail.com>
2021-12-22 15:51:44 +0000

    Altered Contact Hours to Say Sunday: Closed

--- websites/cars/public/admin/deletemanufacturer.php
+++ websites/cars/public/admin/deletemanufacturer.php
@@ -15,7 +15,7 @@ session_start();
 				<h3>Opening Hours:</h3>
 				<p>Mon-Fri: 09:00-17:30</p>
 				<p>Sat: 09:00-17:00</p>
-				<p>Sun: 10:00-16:00</p>
+				<p>Sun: Closed</p>
 			</aside>
 			<img src="/images/logo.png"/>
 
