ba2a6bd4b2dfd649d7e69996a6d6866ee20b5b8c
EmilyF99 <emilyfletcher099@gmail.com>
2021-12-22 21:57:51 +0000

    Removed Redundant code add manufacturer

--- websites/cars/public/addmanufacturer.php
+++ websites/cars/public/addmanufacturer.php
@@ -1,48 +1,28 @@
 <?php
+/*
 $pdo = new PDO('mysql:dbname=cars;host=mysql', 'student', 'student');
 session_start();
-?>
-<!DOCTYPE html>
-<html>
-	<head>
-		<link rel="stylesheet" href="/styles.css"/>
-		<title>Claires's Cars - Home</title>
-	</head>
-	<body>
-	<header>
-		<section>
-			<aside>
-				<h3>Opening Hours:</h3>
-				<p>Mon-Fri: 09:00-17:30</p>
-				<p>Sat: 09:00-17:00</p>
-				<p>Sun: Closed</p>
-			</aside>
-			<img src="/images/logo.png"/>
+?> */
 
+	$title = 'Claires\'s Cars - Admin';
+	$content =
+		'<main class="admin">
+
+		<section class="left">
+			<ul>
+				<li><a href="manufacturers.php">Manufacturers</a></li>
+				<li><a href="cars.php">Cars</a></li>
+			</ul>
 		</section>
-	</header>
-	<nav>
-		<ul>
-			<li><a href="/">Home</a></li>
-			<li><a href="/cars.php">Showroom</a></li>
-			<li><a href="/about.html">About Us</a></li>
-			<li><a href="/contact.php">Contact us</a></li>
-		</ul>
-	</nav>
-		<img src="/images/randombanner.php"/>
-	<main class="admin">
 
-	<section class="left">
-		<ul>
-			<li><a href="manufacturers.php">Manufacturers</a></li>
-			<li><a href="cars.php">Cars</a></li>
+		<section class="right">
+	
+		</section>
+		</main>';
 
-		</ul>
-	</section>
+	require '../templates/layout.html.php';
 
-	<section class="right">
-
-		
+	/*
 	<?php
 
 
@@ -93,14 +73,7 @@ session_start();
 		<?php
 		}
 
-	}
+	} */
 	?>
 
-
-</section>
-	</main>
-	<footer>
-		&copy; Claire's Cars 2018
-	</footer>
-</body>
-</html>
+	
