26873fca1fe32fedd85f4eced948abb1113497ab
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-03 03:10:25 +0000

    Plan for careers page

--- websites/cars/public/clairescareers.php
+++ websites/cars/public/clairescareers.php
@@ -1,11 +1,23 @@
 <?php
+$match = true;
+$title = 'Claire\'s Careers';
+$class = 'jobs';
+$content = '';
 require 'databasejoin.php';
-$content = 
+require '../templates/layout.html.php';
+
+if ($match = true) {
+    /* search through all records */
+    $content = 
+'<h1>Claire\'s Careers</h1>
+<p>Records Found Test</p>';
+}
+
+
+else {
+    $content = 
 '<h1>Claire\'s Careers</h1>
 <p>Claire\'s Cars currently has no job opportunities available, but keep checking as new positions become 
 available regularly!</p>';
-
-$title = 'Claire\'s Careers';
-$class = 'jobs';
-require '../templates/layout.html.php';
+}
 ?>
