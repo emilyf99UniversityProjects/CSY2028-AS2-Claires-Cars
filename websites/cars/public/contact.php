7b359c2bdad903b84f3bf88c260bdd81c3f82713
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-04 15:40:33 +0000

    created template for contact form

--- websites/cars/public/contact.php
+++ websites/cars/public/contact.php
@@ -4,18 +4,7 @@
     require 'openingHours.php';
     
 	$content = 
-	'<form action="contact.php" method="POST" >
-    <h2>Contact Us</h2>
-    <h3>Enter your details below:</h3>
-
-    <label>Name: </label> <input type="text" name = "name" />
-    <label>Email: </label> <input type="text" name = "email" />
-	<label>Telephone: </label> <input type="text" name = "telephone" />
-	<label>Enquiry: </label> <textarea name = "enquiry" > </textarea>
-
-
-    <input type="submit" name="submit" value="Submit" />
-    </form>';
+	loadTemplate('../templates/contact.html.php');
 
     $title = 'Claires\'s Cars - Contact Us';
     $class = 'contact';
---
+++ websites/cars/templates/contact.html.php
@@ -0,0 +1,12 @@
+<form action="contact.php" method="POST" >
+    <h2>Contact Us</h2>
+    <h3>Enter your details below:</h3>
+
+    <label>Name: </label> <input type="text" name = "name" />
+    <label>Email: </label> <input type="text" name = "email" />
+	<label>Telephone: </label> <input type="text" name = "telephone" />
+	<label>Enquiry: </label> <textarea name = "enquiry" > </textarea>
+
+
+    <input type="submit" name="submit" value="Submit" />
+    </form>';
