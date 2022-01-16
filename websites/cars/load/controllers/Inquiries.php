f958a84796f5852b28de1eeb2f4754846289faae
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-16 19:17:27 +0000

    broken contact form

--- websites/cars/load/controllers/Inquiries.php
+++ websites/cars/load/controllers/Inquiries.php
@@ -16,5 +16,26 @@ namespace load\controllers;
             'class' => 'contact'
         ];
     }
+
+    public function contactSubmit() {
+        $inquiries = $this->inquiriesconnect->insert($record);
+        if(isset($_POST['formsubmit'])) {
+            $record = 'contact';
+            $values = [
+                'name' => $_POST['name'],
+                'email' => $_POST['email'],
+                'telephone' => $_POST['telephone'],
+                'inquiry' => $_POST['inquiry']
+                ];  
+        }
+        else {
+            return [
+                'template' => 'contact.html.php',
+                'variables' => ['inquiries' => $inquiries], 
+                'title' => 'Claires\'s Cars - Contact Us',
+                'class' => 'contact'
+            ];  
+        }
+    }
 }
 ?>
