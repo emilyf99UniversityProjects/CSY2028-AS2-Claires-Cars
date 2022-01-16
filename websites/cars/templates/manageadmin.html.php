127acc6e63b574c53bc95b8ce0bc22fffd9253be
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-16 21:29:42 +0000

    Fixes to page

--- websites/cars/templates/manageadmin.html.php
+++ websites/cars/templates/manageadmin.html.php
@@ -4,6 +4,7 @@ require 'leftsectionadmin.html.php';
 <section class="right">
     <h2>Manage Admins</h2>
     <p>Please do not leave this screen on display in public eye as passwords are listed</p>
+    <p><a href ="">Add a New Admin</a></p>
 
     <?php
     foreach ($admins as $admin) {
@@ -11,15 +12,16 @@ require 'leftsectionadmin.html.php';
 	 	echo '<div class="details">';
 		echo '<h4> Username: ' . $admin['username'] . '</h4>';
 		echo '<h4> Password: ' . $admin['password'] . '</h4>';
-        echo '<p><a href ="">Edit</a></p>';
-        echo '<p><a href ="">Delete</a></p>';
+        echo '<p><a href ="">Edit This Admin</a></p>';
+        echo '<p><a href ="">Delete This Admin</a></p>';
 		echo '</div>';
 	 	echo '</li>';
 	 }
 
     if(!$admins) {
     ?>
-    <p>There are currently no Admins in the database, Please try again or contact support.</p>
+    <p>There are currently no Admins in the database, Please Add a Admin or Contact Support.</p>
+    <p><a href ="">Add a New Admin</a></p>
     <?php
     }
     ?>
