13c600f230645bf1fdc8a5c7762ff0eab21792a1
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-24 04:51:38 +0000

    added table layout to completed inq

--- websites/cars/templates/completedinquiries.html.php
+++ websites/cars/templates/completedinquiries.html.php
@@ -3,20 +3,23 @@ require 'leftsectionadmin.html.php';
 ?>
 <section class="right">
     <h2>Completed Inquiries</h2>
-
+    <p>All the Completed Customer inquiries are listed here.</p>
     <?php
     foreach ($inquiries as $inquiry) {
-        echo '<p>All the Completed Customer inquiries are listed here.</p>';
+        echo '<table>';
 		echo '<li>';
 	 	echo '<div class="details">';
-		echo '<h4> Customer Name: ' . $inquiry['name'] . '</h4>';
+        echo '<tr>';
+		echo '<td><h4> Customer Name: ' . $inquiry['name'] . '</h4>';
 		echo '<p> Customer Email: ' . $inquiry['email'] . '</p>';
         echo '<p> Customer Telephone: ' . $inquiry['telephone'] . '</p>';
         echo '<p> Inquiry: ' . $inquiry['inquiry'] . '</p>';
         echo '<p> Complete Date : ' . $inquiry['completeddate'] . '</p>';
-        echo '<p> Complete By : ' . $inquiry['completedby'] . '</p>';
+        echo '<p> Complete By : ' . $inquiry['completedby'] . '</p></td>';
+        echo '</tr>';
 		echo '</div>';
 	 	echo '</li>';
+        echo '</table>';
 	 }
 
     if(!$inquiries) {
