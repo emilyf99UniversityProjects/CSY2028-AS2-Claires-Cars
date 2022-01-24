9f8cc96cfaed7e148e04e203f1b07b91ed423b17
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-24 04:05:27 +0000

    made inquiry manage uneditable

--- websites/cars/templates/completeinquiries.html.php
+++ websites/cars/templates/completeinquiries.html.php
@@ -7,10 +7,10 @@ require 'leftsectionadmin.html.php';
 
     <form action=" " method="POST">
         <input type= "hidden" name= "inquiries[id]" value="<?=$inquiries['id'] ?? ''?>"/>
-        <label>Name: </label><input type = "text" name="inquiries[name]" value=" <?=$inquiries['name'] ?? ''?>"/>
-        <label>Email: </label><input type = "text" name="inquiries[email]" value=" <?=$inquiries['email'] ?? ''?>"/>
-        <label>Telephone: </label><input type = "text" name="inquiries[telephone]" value=" <?=$inquiries['telephone'] ?? ''?>"/>
-        <label>Inquiry: </label><textarea name="inquiries[inquiry]" value=" <?=$inquiries['inquiry'] ?? ''?>"></textarea>
+        <label>Name: </label><input type = "text" name="inquiries[name]" value=" <?=$inquiries['name'] ?? ''?>" readonly="readonly"/>
+        <label>Email: </label><input type = "text" name="inquiries[email]" value=" <?=$inquiries['email'] ?? ''?>" readonly="readonly"/>
+        <label>Telephone: </label><input type = "text" name="inquiries[telephone]" value=" <?=$inquiries['telephone'] ?? ''?>"readonly="readonly"/>
+        <label>Inquiry: </label><textarea name="inquiries[inquiry]" value=" <?=$inquiries['inquiry'] ?? ''?>"readonly="readonly"></textarea>
         <label>Completed: </label><input type = "checkbox" name="inquiries[completed]" value=" <?=$inquiries['completed'] ?? ''?>"/>
         <input type= "hidden" name= "inquiries[completeddate]" value="<?=$inquiries['completeddate'] ?? ''?>"/>
         <input type= "hidden" name= "inquiries[completedby]" value="<?=$inquiries['completedby'] ?? ''?>"/>
