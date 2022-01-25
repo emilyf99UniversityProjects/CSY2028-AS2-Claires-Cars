c40cfabb67d283b3e34e0742c6734b5dc300783a
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-25 22:54:07 +0000

    Inquiry can be toggled

--- websites/cars/templates/completeinquiries.html.php
+++ websites/cars/templates/completeinquiries.html.php
@@ -2,21 +2,8 @@
 require 'leftsectionadmin.html.php';
 ?>
 
-<section class = "right">
-    <h2> Complete Inquiry</h2>
+<section class="right">
+    <h2>Manage Inquiries</h2>
+    <p>Inquiry has been changed</p>
 
-    <form action="" method="POST">
-        <input type= "hidden" name= "inquiries[id]" value="<?=$inquiries['id'] ?? ''?>"/>
-        <label>Name: </label><input type = "text" name="inquiries[name]" value=" <?=$inquiries['name'] ?? ''?>" readonly="readonly"/>
-        <label>Email: </label><input type = "text" name="inquiries[email]" value=" <?=$inquiries['email'] ?? ''?>" readonly="readonly"/>
-        <label>Telephone: </label><input type = "text" name="inquiries[telephone]" value=" <?=$inquiries['telephone'] ?? ''?>"readonly="readonly"/>
-        <label>Inquiry: </label><input type = "text"  name="inquiries[inquiry]" value=" <?=$inquiries['inquiry'] ?? ''?>"readonly="readonly"/>
-        <label>Completed: </label><input type = "checkbox" name="inquiries[completed]" value= 1 />
-                                  <input type = "hidden" name="inquiries[completed]" value= 0 />
-
-        <input type= "submit" name = "submit" value = "Save" />                    
-       <!-- <input type= "hidden" name= "inquiries[completeddate]" value="<?//=$inquiries['completeddate'] ?? ''?>"/> 
-        <input type= "hidden" name= "inquiries[completedby]" value="<//?=$inquiries['completedby'] ?? ''?>"/> -->
-
-    </form>
 </section>
