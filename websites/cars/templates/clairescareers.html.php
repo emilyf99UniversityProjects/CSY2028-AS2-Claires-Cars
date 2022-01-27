801b37cb4c96b3ac85fed112d8137641dc7f0bec
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-27 06:57:42 +0000

    comments added to claires careers

--- websites/cars/templates/clairescareers.html.php
+++ websites/cars/templates/clairescareers.html.php
@@ -1,5 +1,8 @@
+
 <h1>Claire's Careers</h1>
 <?php 
+//the foreach loop is used to generate a display for every job record found in the job table
+//echos are used to pull out each tables attributes
 foreach ($jobs as $job) {
 		echo '<li>';
 	 	echo '<div class="details">';
@@ -10,6 +13,8 @@ foreach ($jobs as $job) {
 		echo '</div>';
 	 	echo '</li>';
 	 }
+
+//if there are not jobs found a message will be displayed, this is to help the user understand why there is no records
 if(!$jobs) {
     ?>
     <p>Claire's Cars currently has no job opportunities available, but keep checking as new positions become 
