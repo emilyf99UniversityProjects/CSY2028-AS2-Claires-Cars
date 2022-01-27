0cf6eff8b60830d9ddc29cd6af0cdf15898cc56d
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-27 23:26:09 +0000

    commented inquiries files

--- websites/cars/templates/completedinquiries.html.php
+++ websites/cars/templates/completedinquiries.html.php
@@ -1,10 +1,14 @@
 <?php
+//calls the admin subnavigation menu
 require 'leftsectionadmin.html.php';
 ?>
 <section class="right">
+    <!--This is outside the foreach loop so it is only called on the top of the page -->
     <h2>Completed Inquiries</h2>
     <p>All the Completed Customer inquiries are listed here.</p>
+
     <?php
+    //for every inquiry found in the database the format below is displayed as a table row
     foreach ($inquiries as $inquiry) {
         echo '<table>';
 		echo '<li>';
@@ -22,6 +26,7 @@ require 'leftsectionadmin.html.php';
         echo '</table>';
 	 }
 
+    //if there are no inquiries a message is displayed to make it easier for the user. 
     if(!$inquiries) {
     ?>
     <p>There are currently no Completed Inquiries in the database.</p>
--- websites/cars/templates/completeinquiries.html.php
+++ websites/cars/templates/completeinquiries.html.php
@@ -1,8 +1,10 @@
 <?php
+//calls the admin sub navigation bar so the user can go back to the admin menu after submission.
 require 'leftsectionadmin.html.php';
 ?>
 
 <section class="right">
+    <!--Display message so the user can understand there changes have been made -->
     <h2>Manage Inquiries</h2>
     <p>Inquiry has been changed</p>
 
