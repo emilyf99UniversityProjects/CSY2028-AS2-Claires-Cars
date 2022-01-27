db6f141521e217f63a4cd4d1f4b7d9244608ba2c
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-27 06:27:00 +0000

    Added comments to admin hub

--- websites/cars/templates/adminhub.html.php
+++ websites/cars/templates/adminhub.html.php
@@ -1,6 +1,10 @@
 <?php
+/*this require is used to load the sub navigation menu for the admins
+It has been seperated out so in the future if this navigation menu is required on a new page it can be easily added.*/
 require 'leftsectionadmin.html.php';
 ?>
+
+<!--This message is displayed to prompt the user on how to use the admin hub. -->
 <section class="right">
 <h2>Admin Hub</h2>
 <p>Use the Menu on the Right to Navigate the Admin Hub</p>
