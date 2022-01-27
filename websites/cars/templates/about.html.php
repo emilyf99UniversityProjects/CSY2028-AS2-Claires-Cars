ed5772f30750ee25afc2a366f5291e8a74f1a6e1
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-27 06:21:11 +0000

    Commented About Template

--- websites/cars/templates/about.html.php
+++ websites/cars/templates/about.html.php
@@ -1,1 +1,2 @@
-<p>Welcome to Claire's Cars, Northampton's specialist in classic and import cars.</p>
+<!--This message displays on the 'About Us' page  -->
+<h3>Welcome to Claire's Cars, Northampton's specialist in classic and import cars.</h3>
