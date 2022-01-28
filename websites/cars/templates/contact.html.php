57098fd9cf80480dfd915fc4a8a52cdae6126c8d
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-28 04:22:21 +0000

    added comments

--- websites/cars/templates/contact.html.php
+++ websites/cars/templates/contact.html.php
@@ -1,9 +1,15 @@
-
+<!-- This form is used so user can post inquiries -->
 <h2>Contact Us</h2>
 <h3>Enter your details below:</h3>
 
 <form action="" method="POST" >
+    <!--Hidden as the user does not need to see the ID.
+        However if the hiddens are not included then form will not post due to missing data 
+        ID is on an auto incrementer-->
     <input type= "hidden" name= "inquiries[id]" value="<?=$inquiries['id'] ?? ''?>"/>
+    <!--completed is hidden as the customer does not need to see this when submitting#
+        This is submitted as 0 as the users enquiry has not been completed, this ensures it is posted to 
+        the right list of the admin -->
     <input type = "hidden" name="inquiries[completed]" value= 0 />
     <label>Name: </label><input type = "text" name="inquiries[name]" value="<?=$inquiries['name'] ?? ''?>" required />
     <label>Email: </label> <input type="text" name = "inquiries[email]" value="<?=$inquiries['email'] ?? ''?>"required />
--- websites/cars/templates/edit.html.php
+++ websites/cars/templates/edit.html.php
@@ -1,5 +1,5 @@
 <!--This is used as a visual prompt for the user 
 without this they may be confused on whether there request was completed -->
 <section class = "right">
-<p> Successfully Deleted</p>
+<p> Your changes have been made.</p>
 </section>
