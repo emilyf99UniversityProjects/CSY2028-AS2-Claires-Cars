6b7b5944ed6958145d263ec515c43634d9b70d09
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-29 21:28:54 +0000

    comments added routes

--- websites/cars/load/Routes.php
+++ websites/cars/load/Routes.php
@@ -1,9 +1,12 @@
 <?php
-   
 	namespace load;
 	class Routes implements \CSY2028\Routes {
     	public function getController($name) {
+
+			//Getting the databas connection
     		require '../databasejoin.php';
+
+			//connecting the database tables to a variable and applying name spaces to them
 
 			$carsconnect = new \CSY2028\DatabaseTable($pdo, 'cars', 'id');
 			$inquiriesconnect = new \CSY2028\DatabaseTable($pdo, 'inquiries', 'id');
@@ -12,6 +15,9 @@
 			$newsconnect = new \CSY2028\DatabaseTable($pdo, 'news', 'id');
 			$manufacturersconnect = new \CSY2028\DatabaseTable($pdo, 'manufacturers', 'id');
 		
+			//creating a controller for each table and creating a new instance of the database in that controller
+			//namesapces are also applied
+	
 			$controllers = [];
 			$controllers['cars'] = new \load\controllers\Cars($carsconnect, $manufacturersconnect);
 			$controllers['inquiries'] = new \load\controllers\Inquiries($inquiriesconnect);
@@ -22,33 +28,28 @@
 			
 
 		return $controllers[$name];
+
 	}	
 
+	/* Where a route cannot be found the user will be sent to news/news 
+	This path is the path for the original index. It has been changed to news/news for clarity of what that page
+	features, news is the controller and news is the home page that displays the news articles. */
 	public function getDefaultRoute() {
     	return 'news/news';
 	}
-
-	//have to be an admin to be logged in 
+ 
+	/* Function is not needed but left in in case future sites need it */
+	/* Can be used to stop users who aren't logged in from accessing pages */
  	public function checkLogin($route) {
  	session_start();
  	$loginRoutes = [];
-	
- 	$loginRoutes['manufacturers/manufacturers'] = true;
- 	$loginRoutes['manufacturers/editManufacturer'] = true;
- 	$loginRoutes['manufacturers/deleteManufacturer'] = true;
 
- 	$loginRoutes['cars/editCars'] = true;
- 	$loginRoutes['cars/deleteCars'] = true;
-
-	 //add careers ones
-	 //add news ones
-	
-	 $requiresLogin = $loginRoutes[$route] ?? false;
+	$requiresLogin = $loginRoutes[$route] ?? false;
 
  		if ($requiresLogin && !isset($_SESSION['loggedin'])) {
     		header('location: /admins/login');
     		exit();  
- 		} 
- 	}
+ 		}  
+ 	}  
 } 
 ?>
