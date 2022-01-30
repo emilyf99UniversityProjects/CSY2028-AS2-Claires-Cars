83b23ea8bae0c297f52037c5a701cdac1a35243f
EmilyF99 <emilyfletcher099@gmail.com>
2022-01-30 18:30:50 +0000

    Changes to fix issues in video

--- websites/cars/templates/adminhub.html.php
+++ websites/cars/templates/adminhub.html.php
@@ -8,6 +8,6 @@ require 'leftsectionadmin.html.php';
 <section class="right">
 <h2>Admin Hub</h2>
 <p>Welcome to the admin hub, you are successfully logged in.</p>
-<p>Use the Menu on the Right to Navigate the Admin Hub</p>
+<p>Use the Menu on the Left to Navigate the Admin Hub</p>
 <p>Please log out when you are done.</p>
 </section>
